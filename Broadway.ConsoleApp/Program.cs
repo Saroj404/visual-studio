e9b1083304cc9552fea7759a57ec8d8267355636
SirOz_Shrest <Shresthasaroj901@gmail.com>
2021-02-14 14:55:55 +0545

    Initial Commit

---
+++ Broadway.ConsoleApp/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using m = Broadway.Project.Models.Logic;
+using Broadway.Project.Models;
+using Broadway.VB;
+using Broadway.Project.Models.Inheritence;
+
+namespace Broadway.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //MathOps();
+            //MathPart2();
+
+            InheritenceExample();
+            Console.ReadLine();
+        }
+
+         public static void MathOps()
+        {
+            Console.Write("Enter the First Number: ");
+            var a = Console.ReadLine();
+            Console.Write("Enter the Second Number: ");
+            var b = Console.ReadLine();
+
+            var num_a = Convert.ToDecimal(a);
+            var num_b = Convert.ToDecimal(b);
+
+            m.Math math = new m.Math();
+
+            Console.WriteLine("Addition "+math.Add(num_a, num_b));
+
+            Console.WriteLine($"Subtract {math.Subtract(num_a, num_b)}");
+
+            Console.WriteLine("Multiplication {0}", math.Multiply(num_a, num_b));
+
+            Console.WriteLine($"Division {math.Divide(num_a, num_b)}");
+
+            //math.Remainder
+
+        }
+
+        public static void MathPart2()
+        {
+            Subject.Math m1 = new Subject.Math();
+            m1.Add(12, 2);
+        }
+
+        static void InheritenceExample()
+        {
+            Transportation t = new Transportation();
+            Transportation lt = new LandTransport();
+            LandTransport lt2 = new LandTransport();
+            t.FunctionOne();
+            lt.FunctionOne();
+            lt2.FunctionOne();
+        }
+    }
+
+}
---
+++ Broadway.Project.Models/Inheritence/Transportation.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Broadway.Project.Models.Inheritence
+{
+    public class Transportation
+    {
+        public Transportation()
+        {
+
+        }
+        public Transportation(string str)
+        {
+            this.str = str;
+        }
+
+        public Transportation(string str, int i)
+        {
+
+        }
+        public Transportation(int i, string str)
+        {
+
+        }
+        protected string str = "test string";
+        protected string MoveStr(string mode)
+        {
+            return "Yay! I can move in "+mode;
+        }
+
+        public virtual void FunctionOne()
+        {
+            Console.WriteLine("I am from base class Transportation");
+        }
+    }
+
+    public class LandTransport : Transportation
+    {
+        public LandTransport() : base()
+        {
+
+        }
+
+        public LandTransport(string str) : base(str)
+        {
+
+        }
+
+        public void Move()
+        {
+            Console.WriteLine(this.MoveStr("land"));
+        }
+
+        public void Move(string str)
+        {
+            this.str = str;
+            
+        }
+
+        public override void FunctionOne()
+        {
+            Console.WriteLine("I am from child class LandTransport");
+        }
+    }
+
+    public class AirTransport : Transportation
+    {
+        public void Move()
+        {
+            Console.WriteLine(this.MoveStr("air"));
+        }
+    }
+
+    public class WaterTransport : Transportation
+    {
+        public void Move()
+        {
+            Console.WriteLine(this.MoveStr("water"));
+        }
+    }
+    public class SpaceTransport : AirTransport
+    {
+        public void Move()
+        {
+            Console.WriteLine(this.MoveStr("space"));
+        }
+    }
+
+
+}
